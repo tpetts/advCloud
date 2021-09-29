fea38c0d68d0412ce84074d5c128d08d4ae59945
Tia Petts <tpetts@madisoncollege.edu>
2021-09-29 17:15:12 -0500

    Putting things into Components
    Created Greeting Component and moved greeting code out of the App.js file and into its own component file.

--- Lab4/react-lab4/src/App.js
+++ Lab4/react-lab4/src/App.js
@@ -1,32 +1,9 @@
 import './App.css';
+import { Greeting } from './Greeting';
 
 const App = () => {
   return (
-    <>
-      <h1>
-        Hello World!
-      </h1>
-
-      <h2>
-        Don't forget to do your diary commits on Github Desktop!
-      </h2>
-
-      <h3>
-        Hello Mika!
-      </h3>
-
-      <h3>
-        Hello Baxter!
-      </h3>
-
-      <h3>
-        Hello Sophie!
-      </h3>
-
-      <h3>
-        Hello Bear!
-      </h3>
-    </>
+      < Greeting />
   );
 }
 
---
+++ Lab4/react-lab4/src/Greeting.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Greeting = () => {
+
+    return(
+
+        <>
+            <h1>
+                Hello World!
+            </h1>
+
+            <h2>
+                Don't forget to do your diary commits on Github Desktop!
+            </h2>
+
+            <h3>
+                Hello Mika!
+            </h3>
+
+            <h3>
+                Hello Baxter!
+            </h3>
+
+            <h3>
+                Hello Sophie!
+            </h3>
+
+            <h3>
+                Hello Bear!
+            </h3>
+        </>
+
+    );
+
+
+};
