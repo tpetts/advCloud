1602890c424bc99df68c70dddcdf12385017c39e
Tia Petts <tpetts@madisoncollege.edu>
2021-09-29 17:00:35 -0500

    App.js
    Added in an <h2> tag and put in a fragment (<>...</>)

--- Lab4/react-lab4/src/App.js
+++ Lab4/react-lab4/src/App.js
@@ -2,9 +2,15 @@ import './App.css';
 
 const App = () => {
   return (
+    <>
     <h1>
       Hello World!
     </h1>
+
+    <h2>
+      Don't forget to do your diary commits on Github Desktop!
+    </h2>
+    </>
   );
 }
 
