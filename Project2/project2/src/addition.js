c01acd683a4ee1edac863ec34579154552bfbf9e
Tia Petts <tpetts@madisoncollege.edu>
2021-10-07 18:19:00 -0500

    Got the Addition Component working...
    Next, I shall make a subtraction component!

--- Project2/project2/src/addition.js
+++ Project2/project2/src/addition.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 export const Addition = (props) => {
-    // let numberOne = "";
-    // let numberTwo = "";
+
     return(
         <>
             <div class="card">
                 <div class="card-body">
-                
+                    <h2>
+                        {props.numberOne + props.numberTwo}
+                    </h2>
 
                 </div>
             </div>
