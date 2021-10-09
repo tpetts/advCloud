e0e7b0a682807203bedf6816fa041cea636c3df0
Tia Petts <tpetts@madisoncollege.edu>
2021-10-09 11:12:53 -0500

    Adding in some colors
    Played around with some styling from bootstrap

--- Project2/project2/src/App.js
+++ Project2/project2/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import {Addition} from './Addition';
 import {Subtraction} from './Subtraction';
@@ -8,23 +8,23 @@ import {Divide} from './Divide';
 function App() {
   return (
     <>
-    <h2>Adding two numbers together:</h2>
+    <h2 class="p-3 mb-2 bg-info text-dark">Adding two numbers together:</h2>
     <Addition numberOne={1} numberTwo={1} />
     <Addition numberOne={10} numberTwo={12} />
     <Addition numberOne={3} numberTwo={0.14159} /> 
     {/* hey look, it's pi! */}
 
-    <h2>Subtracting two numbers:</h2>
+    <h2 class="p-3 mb-2 bg-info text-dark">Subtracting two numbers:</h2>
     <Subtraction numberOne={1} numberTwo={1} />
     <Subtraction numberOne={10} numberTwo={12} />
     <Subtraction numberOne={30} numberTwo={25} />
 
-    <h2>Multiplying two numbers:</h2>
+    <h2 class="p-3 mb-2 bg-info text-dark">Multiplying two numbers:</h2>
     <Multiply numberOne={1} numberTwo={1} />
     <Multiply numberOne={10} numberTwo={12} />
     <Multiply numberOne={30} numberTwo={25} />
 
-    <h2>Dividing two numbers:</h2>
+    <h2 class="p-3 mb-2 bg-info text-dark">Dividing two numbers:</h2>
     <Divide numberOne={280} numberTwo={17} />
     <Divide numberOne={10} numberTwo={5} />
     <Divide numberOne={30} numberTwo={25} />
--- Project2/project2/src/Divide.js
+++ Project2/project2/src/Divide.js
@@ -6,7 +6,7 @@ export const Divide = (props) => {
         <>
             <div class="card">
                 <div class="card-body">
-                    <h2>
+                    <h2 class="p-3 mb-2 bg-info p-2 text-dark bg-opacity-75">
                         {props.numberOne / props.numberTwo}
                     </h2>
 
