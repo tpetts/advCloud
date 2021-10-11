7878bfaeff02f5e43c9c458ab917e556e53c7f07
Tia Petts <tpetts@madisoncollege.edu>
2021-10-10 23:07:42 -0500

    adding code into my app.js
    Calling the API and rendering the data in React

--- lab5/amplify-react-app/src/App.js
+++ lab5/amplify-react-app/src/App.js
@@ -1,10 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { API } from 'aws-amplify';
+import './App.css';
 
 function App() {
+
+  // Create coins variable ans set to empty array
+  const [coins, updateCoins] = useState([])
+
+  // Define function to all API
+  async function fetchCoins() {
+    const data = await API.get('cryptoapi', '/coins')
+    updateCoins(data.coins)
+  }
+
+  // Call fetchCoins function when the component loads
+  useEffect(() => {
+    fetchCoins()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
+
+      {
+        coins.map((coin, index) => (
+          <div key={index}>
+          <h2>{coin.name} - {coin.symbol}</h2>
+          <h5>${coin.price_usd}</h5>
+          </div>
+        ))
+      }
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +45,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
