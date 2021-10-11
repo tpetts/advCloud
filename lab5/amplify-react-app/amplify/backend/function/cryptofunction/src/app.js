92b843dd8519b42e64cc1949c807926b3a633bf9
Tia Petts <tpetts@madisoncollege.edu>
2021-10-11 00:02:40 -0500

    Error
    This is the point that the error was thrown. It was working fine before.

--- lab5/amplify-react-app/amplify/backend/function/cryptofunction/src/app.js
+++ lab5/amplify-react-app/amplify/backend/function/cryptofunction/src/app.js
@@ -30,6 +30,26 @@ app.use(function(req, res, next) {
  * Example get method *
  **********************/
 app.get('/coins', function(req, res) {
+
+  // Define base url
+  let apiUrl = `https://api.coinlore.com/api/tickers?start=0&limit=10`
+
+  // Check if there are any query string parameters
+  // If so, reset the base url to include them
+  if (req.apiGateway && req.apiGateway.event.queryStringParameters) {
+   const { start = 0, limit = 10 } = req.apiGateway.event.queryStringParameters
+   apiUrl = `https://api.coinlore.com/api/tickers/?start=${start}&limit=${limit}`
+  }
+
+  // Call API and return response
+  axios.get(apiUrl)
+    .then(response => {
+      res.json({  coins: response.data.data })
+    })
+    .catch(err => res.json({ error: err }))
+})
+
+
   const coins = [
     { name: 'Bitcoin', symbol: 'BTC', price_usd: "10000" },
     { name: 'Ethereum', symbol: 'ETH', price_usd: "400" },
