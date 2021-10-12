a176c4428fa16a39195045b4182497c991822f19
Tia Petts <tpetts@madisoncollege.edu>
2021-10-11 19:56:24 -0500

    Update introduction.js
    made the introduction to be more personable

--- a-z/a-z-home/src/Introduction.js
+++ a-z/a-z-home/src/Introduction.js
@@ -6,7 +6,9 @@ export const Introduction = () => {
             <h2>About Us</h2>
                     {/* <!-- image of whole family here --> */}
                 <p>
-                    Michael & Paula Moore have been in the business for over 25+ years and absolutely love what they do. They live to help others out. They are both bonded, licensed & insured. There's no job too small for these two to handle! Big or small, we will do it all!
+                    "Michael & Paula Moore have been in the business for over 25+ years and absolutely love what they do. They live to help others out. They are both bonded, licensed & insured. There's no job too small for these two to handle! Big or small, they will do it all!"                         
+                    
+                    - anonymous client, 2021
 
                     {/* <!-- ***mention bonded, licensed & insured*** --> */}
                 </p>
